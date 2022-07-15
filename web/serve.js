75e297ea6fc4495c68ce1a7598b9191512fc07da
nobe4 <nobe4@users.noreply.github.com>
2022-07-15 21:48:49 +0200

    During local development, use a fixed password

--- web/serve.js
+++ web/serve.js
@@ -6,19 +6,13 @@ const { AES } = require("crypto-js");
 const get_env = require("./get_env.js");
 
 const port = 8080;
-const password = get_env("PASSWORD").trim();
 const token = get_env("TOKEN").trim();
 const repository = get_env("REPOSITORY").trim();
+const password = "password";
 const encrypted_repository = AES.encrypt(repository, password).toString();
 const encrypted_token = AES.encrypt(token, password).toString();
 
 serve(function (req, res) {
-  if (req.url == "/") {
-    req.url = "/index.html";
-  }
-
-  console.log(`Serving ${req.url}`);
-
   read(__dirname + req.url, "utf-8", function (err, contents) {
     if (err) {
       res.writeHead(404);
@@ -30,9 +24,10 @@ serve(function (req, res) {
       .replace("ENCRYPTED_TOKEN_TO_INJECT", encrypted_token)
       .replace("ENCRYPTED_REPOSITORY_TO_INJECT", encrypted_repository);
 
-    res.writeHead(200);
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(replaced);
   });
 }).listen(port);
 
 console.log(`Listening on http://localhost:${port}`);
+console.log(`Password is 'password'`);
