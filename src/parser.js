571f5490f122753de9802730da640b563aa1f1d3
nobe4 <nobe4@users.noreply.github.com>
2022-09-10 09:14:52 +0200

    Move parser logic into a separate file

---
+++ src/parser.js
@@ -0,0 +1,131 @@
+// Handle parsing the RSS feed.
+
+const core = require("@actions/core");
+const { XMLParser } = require("fast-xml-parser");
+
+const self = {
+  // parse_item extract a number of information from the item.
+  // It makes RSS/ATOM into a single type of object.
+  //
+  // @param {object} item - The item to extract the content from.
+  // @param {object} source - The source this item is coming from.
+  //
+  // @return {object} The parsed item.
+  // Format:
+  //        {
+  //          source: {
+  //            name: '<NAME>',
+  //            url: '<URL>',
+  //            rss_url: '<URL>',
+  //          },
+  //          id: '<ID>',
+  //          link: '<LINK>',
+  //          title: '<TITLE>',
+  //          content: '<CONTENT>',
+  //          published: <TIMESTAMP>
+  //        }
+  parse_item(item, source) {
+    return {
+      source: source,
+      id: item.guid || item.id,
+      title: item.title,
+      content: self.parse_content(item),
+      link: self.parse_link(item),
+      published: self.parse_published(item),
+    };
+  },
+
+  // parse uses an XML string to extract information about feed items.
+  // It handles RSS AND ATOM feeds
+  // ref: https://en.wikipedia.org/wiki/RSS#RSS_compared_with_Atom
+  //
+  // @param {string} data - The string data containing the feed in XML.
+  // @param {object} source - The source this data is coming from.
+  //
+  // @return {Promise} - Resolve with the parsed items.
+  //                     Reject with any error that occured.
+  parse(data, source) {
+    return new Promise((resolve, reject) => {
+      const parser = new XMLParser();
+      const result = parser.parse(data);
+
+      // Get the channel
+      // - `rss.channel` for RSS feeds
+      // - `feed` for Atom feeds
+      let channel =
+        result.rss && result.rss.channel ? result.rss.channel : result.feed;
+
+      // If channel is undefined, it means data wasn't XML, reject.
+      if (channel == undefined) {
+        reject(`invalid XML for '${source.name}' '${source.rss_url}'\n${data}`);
+        return;
+      }
+
+      // Select the first channel only.
+      if (Array.isArray(channel)) channel = channel[0];
+
+      // RSS items are in `item`, Atom items are in `entry`.
+      let items = channel.item || channel.entry;
+
+      // Ensure we have an array of items.
+      if (items && !Array.isArray(items)) items = [items];
+
+      // If there is no item, it can be OK, but it's better to warn.
+      if (!items || items.length == 0) {
+        core.warning(`No items found for '${source.name}'.`);
+        resolve([]);
+      }
+
+      // Parse each items
+      const parsed_items = items.map((item) => {
+        return self.parse_item(item, source);
+      });
+
+      resolve(parsed_items);
+    });
+  },
+
+  // parse_content tries to get the content from the item.
+  // Since we're parsing RSS and ATOM, it can be in a number of places.
+  // Also merge `description` and `content`.
+  //
+  // @param {object} item - The item to extract the content from.
+  //
+  // @return {string} - The content to use.
+  parse_content(item) {
+    if (item.description) return item.description;
+    if (item.summary) return item.summary;
+    if (item.content) return item.content;
+    if (item["media:group"] && item["media:group"]["media:description"])
+      return item["media:group"]["media:description"];
+  },
+
+  // parse_published tries to get the published date from the item.
+  // Since we're parsing RSS and ATOM, it can be in a number of places.
+  // It defaults to the current date and return a parsed date.
+  //
+  // @param {object} item - The item to extract the published date from.
+  //
+  // @return {date} - The published date to use.
+  parse_published(item) {
+    if (item.pubDate) return Date.parse(item.pubDate);
+    if (item.created) return Date.parse(item.created);
+    return Date.now();
+  },
+
+  // parse_link tries to get the link date from the item.
+  //
+  // @param {object} item - The item to extract the link from.
+  //
+  // @return {date} - The link to use.
+  parse_link(item) {
+    if (item.link && item.link != "") return item.link;
+
+    // Youtube doesn't pass the link directly, but the video id
+    if (item["yt:videoId"]) {
+      return `https://www.youtube.com/watch?v=${item["yt:videoId"]}`;
+    }
+  },
+};
+
+module.exports = self;
---
+++ tests/parser.test.js
@@ -0,0 +1,200 @@
+const core = require("@actions/core");
+jest.mock("@actions/core");
+
+const fs = require("fs");
+const { XMLParser } = require("fast-xml-parser");
+
+const parser = require("../src/parser.js");
+
+const source = {
+  name: "name",
+  url: "url",
+  rss_url: "rss_url",
+};
+
+describe("parse_content", () => {
+  [
+    { description: "content" },
+    { summary: "content" },
+    { content: "content" },
+    { "media:group": { "media:description": "content" } },
+  ].forEach((item) => {
+    it(`works for ${JSON.stringify(item)}`, () => {
+      expect(parser.parse_content(item)).toBe("content");
+    });
+  });
+
+  [{}, { "media:group": "content" }].forEach((item) => {
+    it(`is undefined for ${JSON.stringify(item)}`, () => {
+      expect(parser.parse_content(item)).toBeUndefined();
+    });
+  });
+});
+
+describe("parse_published", () => {
+  const today = "2022-01-01";
+  const parsed_today = Date.parse(today);
+  jest.useFakeTimers().setSystemTime(parsed_today);
+
+  [{ created: today }, { pubDate: today }, { no_date: true }].forEach(
+    (item) => {
+      it(`works for ${JSON.stringify(item)}`, () => {
+        expect(parser.parse_published(item)).toBe(parsed_today);
+      });
+    }
+  );
+});
+
+describe("parse_link", () => {
+  [
+    { item: { link: "link" }, expected: "link" },
+    { item: { link: "" }, expected: undefined },
+    { item: { not_link: "link" }, expected: undefined },
+    {
+      item: { "yt:videoId": "id" },
+      expected: "https://www.youtube.com/watch?v=id",
+    },
+  ].forEach((t) => {
+    it(`works for ${JSON.stringify(t.item)}`, () => {
+      expect(parser.parse_link(t.item)).toBe(t.expected);
+    });
+  });
+});
+
+describe("parse_item", () => {
+  const expected = {
+    source: source,
+    id: "id",
+    title: "title",
+    link: "link",
+    content: "content",
+    published: "published",
+  };
+
+  const tests = [
+    {
+      guid: "id",
+      title: "title",
+      link: "link",
+    },
+    {
+      id: "id",
+      title: "title",
+      link: "link",
+    },
+  ];
+
+  tests.forEach((item) => {
+    it(`works for ${JSON.stringify(item)}`, () => {
+      jest.spyOn(parser, "parse_content").mockReturnValueOnce("content");
+      jest.spyOn(parser, "parse_published").mockReturnValueOnce("published");
+
+      expect(parser.parse_item(item, source)).toEqual(expected);
+    });
+  });
+});
+
+describe("parse", () => {
+  it("rejects for invalid xml", () => {
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockImplementationOnce((raw_data) => {
+        expect(raw_data).toBe("raw_data");
+        // Invalid XML produces an empty object
+        return {};
+      });
+
+    expect(parser.parse("raw_data", source)).rejects.toMatch("invalid XML");
+
+    // This time without the mock
+    expect(parser.parse("raw_data", source)).rejects.toMatch("invalid XML");
+  });
+
+  it("calls parser.parse_item on each found item in an RSS feed", () => {
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockReturnValueOnce({ rss: { channel: { item: [1, 2, 3] } } });
+
+    jest
+      .spyOn(parser, "parse_item")
+      .mockReturnValueOnce("parsed_1")
+      .mockReturnValueOnce("parsed_2")
+      .mockReturnValueOnce("parsed_3");
+
+    expect(parser.parse("data", source)).resolves.toStrictEqual([
+      "parsed_1",
+      "parsed_2",
+      "parsed_3",
+    ]);
+  });
+
+  it("calls parser.parse_item on each found item in an ATOM feed", () => {
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockReturnValueOnce({ feed: { entry: [1, 2, 3] } });
+
+    jest
+      .spyOn(parser, "parse_item")
+      .mockReturnValueOnce("parsed_1")
+      .mockReturnValueOnce("parsed_2")
+      .mockReturnValueOnce("parsed_3");
+
+    expect(parser.parse("data", source)).resolves.toStrictEqual([
+      "parsed_1",
+      "parsed_2",
+      "parsed_3",
+    ]);
+  });
+
+  it("works if there's multiple channels", () => {
+    // 2nd and 3rd channels are going to be ignored, so we can leave them empty
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockReturnValueOnce({ rss: { channel: [{ item: [1] }, {}, {}] } });
+
+    jest.spyOn(parser, "parse_item").mockReturnValueOnce("parsed_1");
+    expect(parser.parse("data")).resolves.toStrictEqual(["parsed_1"]);
+  });
+
+  it("works if there's only one item", () => {
+    // 2nd and 3rd channels are going to be ignored, so we can leave them empty
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockReturnValueOnce({ rss: { channel: { item: 1 } } });
+
+    jest.spyOn(parser, "parse_item").mockReturnValueOnce("parsed_1");
+    expect(parser.parse("data")).resolves.toStrictEqual(["parsed_1"]);
+  });
+
+  it("works if there's no item", () => {
+    jest
+      .spyOn(XMLParser.prototype, "parse")
+      .mockReturnValueOnce({ rss: { channel: { item: [] } } });
+
+    expect(parser.parse("data", { name: "name" })).resolves.toStrictEqual([]);
+    expect(core.warning).toHaveBeenCalledWith("No items found for 'name'.");
+  });
+
+  it("works for an XML document", () => {
+    const rss_feed = fs.readFileSync("tests/fixtures/rss_feed.xml").toString();
+
+    parser.parse(rss_feed).then((data) => {
+      expect(data).toHaveLength(2);
+      expect(data[0].published).toBe(1654079316000);
+      expect(data[1].published).toBe(1654078660000);
+    });
+  });
+
+  it("works for an XML document", () => {
+    const atom_feed = fs
+      .readFileSync("tests/fixtures/atom_feed.xml")
+      .toString();
+
+    parser.parse(atom_feed).then((data) => {
+      expect(data).toHaveLength(2);
+      expect(data[0].published).toBe(1640995200000);
+      expect(data[1].published).toBe(1640995200000);
+    });
+  });
+});
+
